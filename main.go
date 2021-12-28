70f9e292f9034f9597743399e3914d9fffca3674
Joaquin Ribero <joaquin.ribero@mercadolibre.com>
2021-12-28 15:40:45 -0300

    Add swagger

--- main.go
+++ main.go
@@ -2,14 +2,26 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/JoaquinRibero/go-web/cmd/server/controller"
+	"github.com/JoaquinRibero/go-web/docs"
 	"github.com/JoaquinRibero/go-web/internal/transactions"
 	"github.com/JoaquinRibero/go-web/pkg/store"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
+	ginSwagger "github.com/swaggo/gin-swagger"
+	"github.com/swaggo/gin-swagger/swaggerFiles"
 )
 
+// @title MELI Bootcamp API
+// @version 1.0
+// @description Transactions API ML Bootcamp GO
+// @contact.name API Support
+// @contact.email joaquin.ribero@mercadolibre.com
+// @host localhost:8080
+// @BasePath /transactions/
+// @securityDefinitions.basic BasicAuth
 func main() {
 	err := godotenv.Load()
 	if err != nil {
@@ -21,6 +33,9 @@ func main() {
 	t := controller.NewTransaction(service)
 
 	r := gin.Default()
+	docs.SwaggerInfo.Host = os.Getenv("HOST")
+	r.GET("/docs/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
 	tr := r.Group("/transactions", t.ValidateToken())
 	tr.GET("/", t.GetAll())
 	tr.POST("/new", t.NewUser())
