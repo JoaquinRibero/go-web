99c8e8cbf6e12c0669e557e1b4d71d32fca6c232
Joaquin Ribero <joaquin.ribero@mercadolibre.com>
2021-12-28 11:51:37 -0300

    Web pkg, struct responses

--- cmd/server/controller/controller.go
+++ cmd/server/controller/controller.go
@@ -7,6 +7,7 @@ import (
 	"strconv"
 
 	"github.com/JoaquinRibero/go-web/internal/transactions"
+	"github.com/JoaquinRibero/go-web/pkg/web"
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
 )
@@ -21,28 +22,36 @@ func NewTransaction(t transactions.Service) *Transaction {
 	}
 }
 
-func Mensaje(verr validator.ValidationErrors) map[string]string {
-	errs := make(map[string]string)
+func Mensaje(verr validator.ValidationErrors) []string {
+	//errs := make(map[string]string)
+	var errors []string
 	for _, f := range verr {
-		errs[f.Field()] = fmt.Sprintf("El campo %s es requerido", f.Field())
+		//errs[f.Field()] = fmt.Sprintf("El campo %s es requerido", f.Field())
+		errors = append(errors, fmt.Sprintf("El campo %s es requerido", f.Field()))
 	}
-	return errs
+	return errors
 }
 
 func (t *Transaction) ValidateToken() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		token := ctx.GetHeader("token")
 		tokenEnv := os.Getenv("TOKEN")
-		if token != tokenEnv {
-			ctx.JSON(401, gin.H{"errors": "no tiene permisos para realizar la peticion solicitada"})
+		if token == "" {
+			ctx.AbortWithStatusJSON(401, gin.H{"error": "API token is required"})
+			return
 		}
+		if token != tokenEnv {
+			ctx.AbortWithStatusJSON(401, gin.H{"errors": "Unauthorized. Please add a valid API token"})
+			return
+		}
+		ctx.Next()
 	}
 }
 
 func (c *Transaction) GetAll() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		t := c.service.GetAll()
-		ctx.JSON(200, t)
+		ctx.JSON(200, web.NewResponse(200, t, []string{""}))
 	}
 }
 
@@ -60,15 +69,15 @@ func (c *Transaction) NewUser() gin.HandlerFunc {
 		if err := ctx.ShouldBindJSON(&req); err != nil {
 			var verr validator.ValidationErrors
 			if errors.As(err, &verr) {
-				ctx.JSON(400, gin.H{"errors": Mensaje(verr)})
+				ctx.JSON(400, web.NewResponse(400, nil, Mensaje(verr)))
 				return
 			}
 		} else {
 			t, err := c.service.NewUser(req.Codigo, req.Moneda, req.Monto, req.Emisor, req.Receptor, req.Fecha)
 			if err != nil {
-				ctx.JSON(401, gin.H{"error": err.Error()})
+				ctx.JSON(401, web.NewResponse(401, nil, []string{err.Error()}))
 			}
-			ctx.JSON(200, t)
+			ctx.JSON(200, web.NewResponse(200, t, []string{}))
 		}
 
 	}
@@ -88,21 +97,21 @@ func (c *Transaction) Update() gin.HandlerFunc {
 		if err := ctx.ShouldBindJSON(&req); err != nil {
 			var verr validator.ValidationErrors
 			if errors.As(err, &verr) {
-				ctx.JSON(400, gin.H{"errors": Mensaje(verr)})
+				ctx.JSON(400, web.NewResponse(400, nil, Mensaje(verr)))
 				return
 			}
 		}
 		id, err := strconv.ParseInt(ctx.Param("id"), 10, 64)
 		if err != nil {
-			ctx.JSON(400, gin.H{"error": "invalid ID"})
+			ctx.JSON(400, web.NewResponse(400, nil, []string{"invalid ID"}))
 			return
 		}
 
 		t, err := c.service.Update(int(id), req.Codigo, req.Moneda, req.Monto, req.Emisor, req.Receptor, req.Fecha)
 		if err != nil {
-			ctx.JSON(404, gin.H{"error": err.Error()})
+			ctx.JSON(404, web.NewResponse(404, nil, []string{err.Error()}))
 		}
-		ctx.JSON(200, t)
+		ctx.JSON(200, web.NewResponse(200, t, []string{}))
 	}
 }
 
@@ -110,14 +119,14 @@ func (t *Transaction) Delete() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		id, err := strconv.ParseInt(ctx.Param("id"), 10, 64)
 		if err != nil {
-			ctx.JSON(400, gin.H{"error": "invalid ID"})
+			ctx.JSON(400, web.NewResponse(400, nil, []string{"invalid ID"}))
 			return
 		}
 		err = t.service.Delete(int(id))
 		if err != nil {
-			ctx.JSON(404, gin.H{"error": err.Error()})
+			ctx.JSON(404, web.NewResponse(404, nil, []string{err.Error()}))
 		}
-		ctx.JSON(200, gin.H{"message": "success"})
+		ctx.JSON(200, web.NewResponse(200, nil, []string{}))
 	}
 }
 
@@ -131,19 +140,19 @@ func (t *Transaction) UpdatePartial() gin.HandlerFunc {
 		if err := ctx.ShouldBindJSON(&req); err != nil {
 			var verr validator.ValidationErrors
 			if errors.As(err, &verr) {
-				ctx.JSON(400, gin.H{"errors": Mensaje(verr)})
+				ctx.JSON(400, web.NewResponse(400, nil, Mensaje(verr)))
 				return
 			}
 		}
 		id, err := strconv.ParseInt(ctx.Param("id"), 10, 64)
 		if err != nil {
-			ctx.JSON(400, gin.H{"error": "invalid ID"})
+			ctx.JSON(400, web.NewResponse(400, nil, []string{"invalid ID"}))
 			return
 		}
 		t, err := t.service.UpdateCodigoAndMonto(int(id), req.Codigo, req.Monto)
 		if err != nil {
-			ctx.JSON(404, gin.H{"error": err.Error()})
+			ctx.JSON(404, web.NewResponse(404, nil, []string{err.Error()}))
 		}
-		ctx.JSON(200, t)
+		ctx.JSON(200, web.NewResponse(200, t, []string{}))
 	}
 }
